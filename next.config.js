8e4b4c2ac8f1fafe4e0bd73c7ebd021637f0e4ac
公众号：吾爱共享 <70825982+hanxizu@users.noreply.github.com>
2025-08-24 20:23:26 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['@resvg/resvg-wasm']
+  },
+  webpack: (config) => {
+    // 允许 WebAssembly (WASM) 在构建中使用
+    config.experiments = {
+      ...(config.experiments || {}),
+      asyncWebAssembly: true,
+      layers: true
+    };
+    return config;
+  }
 };
 
-module.exports = nextConfig;
+export default nextConfig;
